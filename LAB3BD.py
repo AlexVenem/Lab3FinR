a2fdbeddaa1b6ffc4e2fdc27708fa4e11d1c42aa
AlexVenem <130144087+AlexVenem@users.noreply.github.com>
2023-12-25 02:56:58 +0300

    Update LAB3BD.py

--- LAB3BD.py
+++ LAB3BD.py
@@ -1,1 +1,14 @@
+from benches import *
+from config import *
 
+
+
+run = {
+    'sqlite': sqlite,
+    'pandas': pandas,
+    'psycopg2': psycopg2,
+    'duckdb': duckdb,
+    'sqlalchemy': sqlalchemy,
+    }
+
+run[LIB]()
